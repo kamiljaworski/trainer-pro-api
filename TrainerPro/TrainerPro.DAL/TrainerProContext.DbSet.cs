78584ca27fbbdbbd5a214e7119d28763af1acd7f
Kamil Jaworski <kamilj610@gmail.com>
2019-10-07 10:05:16 +0200

    Create project

---
+++ TrainerPro/TrainerPro.DAL/TrainerProContext.DbSet.cs
@@ -0,0 +1,10 @@
+﻿namespace TrainerPro.DAL
+{
+    /// <summary>
+    /// Partial class that contains only DbSets e.g
+    /// public DbSet<Test> Tests { get; set; }
+    /// </summary>
+    public partial class TrainerProContext
+    {
+    }
+}
---
+++ TrainerPro/TrainerPro.DAL/TrainerProContext.cs
@@ -0,0 +1,15 @@
+﻿namespace TrainerPro.DAL
+{
+    using Microsoft.EntityFrameworkCore;
+
+    /// <summary>
+    /// Partial class that contains EF DbContext configuration
+    /// </summary>
+    public partial class TrainerProContext : DbContext
+    {
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
